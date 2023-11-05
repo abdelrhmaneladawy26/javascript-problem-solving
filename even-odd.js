eb723377441a2df381190de33496aab9b1357bd7
abdelrhmaneladawy26 <abdelrhmaneladawy26@gmail.com>
2023-11-05 09:34:10 +0200

    [2] even-odd

--- even-odd.js
+++ even-odd.js
@@ -1,4 +1,4 @@
-// [1] Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// [1] Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers
 
 function checkEvenOrOdd(number) {
   return number % 2 == 0 ? "Even" : "Odd";
